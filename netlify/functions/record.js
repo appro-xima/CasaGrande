a8e3d22245674070c3c59bf468fdecec786f4bf6
appro-xima <mfvotarra@tip.edu.ph>
2024-12-04 02:04:54 +0800

    Update record.js

--- netlify/functions/record.js
+++ netlify/functions/record.js
@@ -4,7 +4,7 @@ exports.handler = async (event, context) => {
     if (event.httpMethod === "POST") {
         const data = JSON.parse(event.body); // Parse the incoming JSON data
 
-        // Simulate processing the data, similar to what confirm.php would do
+        // Simulate processing the data
         const confirmationMessage = `Reservation confirmed for ${data.name} with email ${data.email}. Check-in: ${data.checkin}, Check-out: ${data.checkout}, Room Type: ${data.roomType}, Room Number: ${data.roomCode}.`;
 
         return {
